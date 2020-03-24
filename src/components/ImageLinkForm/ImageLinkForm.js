6aeeec2310401cf1dca93a604f0c6b1077e2a67b
cumaniar <chiragma18633@gmail.com>
2020-03-24 19:08:18 +0530

    placholder in input added

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,20 +1,32 @@
-import React from 'react';
-import './ImageLinkForm.css'
+import React from "react";
+import "./ImageLinkForm.css";
 
 const ImageLinkForm = ({ onInputChange, onButtonClick }) => {
-    return (
-        <div>
-            <p className='f3'>
-                {'This magic brain will detect your brain, Give it a chance!'}
-            </p>
-            <div className='center'>
-                <div className='pa4 br3 shadow-5 form'>
-                    <input className="w-70 f4" type='text' name='url' onChange={onInputChange} />
-                    <button className='w-30  grow f4 link ph3 pv2 dib white bg-light-purple' onClick={onButtonClick}> Detect </button>
-                </div>
-            </div>
+  return (
+    <div>
+      <p className="f3">
+        {"This magic brain will detect your brain, Give it a chance!"}
+      </p>
+      <div className="center">
+        <div className="pa4 br3 shadow-5 form">
+          <input
+            className="w-70 f4"
+            type="text"
+            name="url"
+            onChange={onInputChange}
+            placeholder="Enter image url (Not more than 200 characters)"
+          />
+          <button
+            className="w-30  grow f4 link ph3 pv2 dib white bg-light-purple"
+            onClick={onButtonClick}
+          >
+            {" "}
+            Detect{" "}
+          </button>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default ImageLinkForm;
